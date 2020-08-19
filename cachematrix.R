ab712dc08de8df9b23b1dec4e45bce3ec8b59784
lena-b <lena.boehnke@uibk.ac.at>
2020-08-19 10:45:22 +0200

    assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        i <- NULL                               
+        set <- function(y){                             #set value of matrix with FUN y
+                x <<- y                                 #<<- to assign value to object in an environment different from current env
+                i <<- NULL
+        }
+        get <- function(){x}                            #get value of the matrix
+        setInverse <- function(inverse) i <<- inverse   #set value of inverse
+        getInverse <- function() i                      #gets value of inverse
+        list(set = set, get = get,                      #create list to display values
+             setInverse = setInverse, 
+             getInverse = getInverse)
 }
 
 
@@ -12,4 +22,13 @@ makeCacheMatrix <- function(x = matrix()) {
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        i <- x$getInverse()                             #returns matrix inverse of x and assigns it to i
+        if(!is.null(i)){                                #when i is coming from chache message "getting cached data" is returned
+                message("getting cached data")
+                return(i)
+        }
+        mat <- x$get()
+        i <- solve(mat,...)
+        x$setInverse(i)                                 #set value of inverse in the cache with setInverse
+        i
 }
